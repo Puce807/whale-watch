cb4174179b05d87ff0d7c93a9adb4fe735fc3a60
Puce807 <luke.clarkeson@gmail.com>
2026-01-25 10:51:00 +0000

    Basic argument passing using click

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#Initial Commit 
+import click
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option("--count", default=1, help="Number of greetings")
+@click.option("--name", prompt="Your name", help="The person to greet")
+def test(count, name):
+    for i in range(count):
+        print(f"Hello {name}")
+
+if __name__ == "__main__":
+    cli()
