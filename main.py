351e11d8f5d1bbb3f8531d7dfc894e84308b8e0f
Puce807 <luke.clarkeson@gmail.com>
2026-01-25 11:41:30 +0000

    Updated ls function to make output more readable

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ def update():
 
 @cli.command()
 def ls():
-    print(list_containers(client))
+    for container, info in named_containers.items():
+        print(f"{named_containers}: Status {info["status"]} | ID {info["id"]}")
 
 if __name__ == "__main__":
     if AUTO_GIT_PULL: update_git()
     client = docker.from_env()
+
+    containers = list_containers(cli)
+    named_containers = {info["name"]: {"id": c_id, "status": info["status"]} for c_id, info in containers.items()}
+
     cli()
