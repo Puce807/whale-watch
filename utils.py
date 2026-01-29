210a4c52bb25336275d3bf25be4f25bb9ad307d9
Puce807 <luke.clarkeson@gmail.com>
2026-01-29 19:29:49 +0000

    [0.3.6] Fix: Corrected error with updating git

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ def check_git():
         return False # New commits
 
 def update_git():
-    result = subprocess.run(["git", "pull"], capture_output=True)
+    result = subprocess.run(["git", "pull"], capture_output=True, text=True)
     if "Already up to date" not in result:
         log("Run script again for update to take affect", 1)
 
